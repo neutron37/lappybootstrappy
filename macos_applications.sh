a7b0b56d5892993b6bfbf3108763232054b682b9
tstec <tom.stec@polypoly.eu>
2020-11-22 17:01:10 +0100

    Applications latest and greatest.

--- macos_applications.sh
+++ macos_applications.sh
@@ -5,18 +5,6 @@ brew cask install iterm2
   # Install iterm2 zsh shell integration
   curl -q -L https://iterm2.com/shell_integration/zsh \
     -o ~/.iterm2_shell_integration.zsh
-  # Install imgcat
-  curl -s -L https://www.iterm2.com/utilities/imgcat \
-    -o ~/.local/bin/imgcat
-  chmod +x ~/.local/bin/imgcat
-  # Install imgls
-  curl -s -L https://www.iterm2.com/utilities/imgls \
-    -o ~/.local/bin/imgls
-  chmod +x ~/.local/bin/imgls
-  # Install divider
-  curl -s -L https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/divider \
-    -o ~/.local/bin/divider
-  chmod +x ~/.local/bin/divider
 
 # zsh
 brew install zsh
@@ -34,14 +22,11 @@ brew install zsh
   # https://unix.stackexchange.com/questions/210930/completions-stopped-working-after-upgrading-zsh/210931#210931
   rm ~/.zcompdump*
 
-
-# atom
-brew cask install atom
-open /Applications/Atom.app
-
 # VSCode
 # brew cask install visual-studio-code
 # open /Applications/Visual\ Studio\ Code.app
+brew cask install vscodium
+open /Applications/VSCodium.app
 
 # ShiftIt
 brew cask install shiftit
@@ -59,6 +44,12 @@ open /Applications/Docker.app
 # Chrome
 brew cask install google-chrome
 open /Applications/Google\ Chrome.app
+
+# Slack
+brew cask install Slack
+
+# GPG Suite
+brew cask install gpg-suite
 
 # Vagrant
 brew cask install vagrant
@@ -103,7 +94,7 @@ open /Applications/Alfred\ 4.app
 open https://github.com/stuartcryan/custom-iterm-applescripts-for-alfred/blob/master/custom_iterm_script_iterm_3.1.1.applescript
 
 # Install useful commands
-brew install wget ripgrep loc watch most telnet
+brew install wget ripgrep loc watch most nmap netcat telnet scc
 
 # Install PHP stuff
 brew install php@7.1 composer
@@ -117,6 +108,11 @@ brew cask install balenaetcher
 # Install signal
 brew cask install signal
 
+# Install taskwarrior
+brew install task
+brew install taskd
+brew install tasksh
+
 # Install localtunnel
 npm install -g localtunnel
 
@@ -126,29 +122,60 @@ brew cask install spotify
 # Install Thunderbird
 brew cask install thunderbird
 
+# Install kind and minikube
+brew install kind
+brew install minikube
+
+# Install yubico tools
+brew cask install homebrew/cask-drivers/yubico-authenticator
+brew cask install homebrew/cask-drivers/yubico-yubikey-manager
+brew install ykman
+
+# Update slot two for slower typing
+# ykman otp settings -p20 2
+
+# Install karabiner
+brew cask install karabiner-elementss
+
+# Install postman
+brew cask install postman
+
+# Install firefox
+brew cask install firefox
+
 # Install Tutanota
 brew cask install tutanota
 
-# Install Vitamin-R
-brew cask install vitamin-r
-open '/Applications/Vitamin-R 3.app'
-atom ~/Documents/licenses/vitamin-r.license.yaml
+# Install min.io client
+brew install minio/stable/mc
 
 # Install Ableton 9 Suite
 brew tap homebrew/cask-versions
 brew cask install ableton-live-suite9
+
+# Install tmate (terminal multiplexer)
+brew install tmate
 
 # Install Little Snitch
 brew cask install little-snitch
 open '/usr/local/Caskroom/little-snitch/4.4.2/LittleSnitch-4.4.2.dmg'
 sleep 10
 open '/Volumes/Little Snitch 4.4.2/Little Snitch Installer.app'
-atom ~/Documents/licenses/little-snitch.license
+code ~/Documents/licenses/little-snitch.license
 
 # Install Micro snitch
 brew cask install micro-snitch
 open '/Applications/Micro Snitch.app'
-atom ~/Documents/licenses/micro-snitch-dual.license
+code ~/Documents/licenses/micro-snitch-dual.license
+
+# Install testssl.sh
+brew install testssl
+
+# Install npm
+brew install npm
+
+# Install git-secret
+brew install git-secret
 
 # Install Kindle
 brew cask install kindle
@@ -156,6 +183,17 @@ brew cask install kindle
 # Install JAVA (OpenJDK)
 brew cask install java
 
-# Install Symfony
-curl -sS https://get.symfony.com/cli/installer | bash
-mv /Users/user1/.symfony/bin/symfony /usr/local/bin/symfony
+# Install font inconsolata
+brew tap homebrew/cask-fonts
+brew cask install font-inconsolata
+brew cask install font-inconsolata-for-powerline
+
+# Zettlr notebook
+brew cask install Zettlr
+
+# # Install Symfony
+# curl -sS https://get.symfony.com/cli/installer | bash
+# mv /Users/user1/.symfony/bin/symfony /usr/local/bin/symfony
+
+# Install hyperkit
+brew install hyperkit
